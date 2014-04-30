bdfb4fc07873e2714e041fcd9f7500b1872a2422
rabbitfighter <b_wright@b-wright-macbook2.local>
2014-04-30 08:37:41 -0700

    made deutilize() function better

--- deutilize.js
+++ deutilize.js
@@ -1,107 +1,22 @@
-// there is obviously a better way to do this
-// I will find it later
 
 function deutilize() {
 	
-	var find = "utilize";
-	var repl = "use";
-	var page = document.body.innerHTML;
-	while (page.indexOf(find) >= 0) {
-		var i = page.indexOf(find);
-		var j = find.length;
-		page = page.substr(0,i) + repl + page.substr(i+j);
-		document.body.innerHTML = page;
-	}
+	var utilize = ["utilize","Utilize","utilise","Utilise","utilizing","Utilizing","utilizable","Utilizable","utilization","Utilization"];
+	var use = ["use","Use","use","Use","using","Using","usable","Usable","use","Use"];
+
+	for (var x = 0; x < utilize.length; x++) {
+		var find = utilize[x];
+		var repl = use[x];
+		var page = document.body.innerHTML;
+		while (page.indexOf(find) >= 0) {
+			var i = page.indexOf(find);
+			var j = find.length;
+			page = page.substr(0,i) + repl + page.substr(i+j);
+			document.body.innerHTML = page;
+		}
+	}	
 	
-	var find = "Utilize";
-	var repl = "Use";
-	var page = document.body.innerHTML;
-	while (page.indexOf(find) >= 0) {
-		var i = page.indexOf(find);
-		var j = find.length;
-		page = page.substr(0,i) + repl + page.substr(i+j);
-		document.body.innerHTML = page;
-	}
-	
-	var find = "utilise";
-	var repl = "use";
-	var page = document.body.innerHTML;
-	while (page.indexOf(find) >= 0) {
-		var i = page.indexOf(find);
-		var j = find.length;
-		page = page.substr(0,i) + repl + page.substr(i+j);
-		document.body.innerHTML = page;
-	}
-	
-	var find = "Utilise";
-	var repl = "Use";
-	var page = document.body.innerHTML;
-	while (page.indexOf(find) >= 0) {
-		var i = page.indexOf(find);
-		var j = find.length;
-		page = page.substr(0,i) + repl + page.substr(i+j);
-		document.body.innerHTML = page;
-	}
-	
-	var find = "utilizing";
-	var repl = "using";
-	var page = document.body.innerHTML;
-	while (page.indexOf(find) >= 0) {
-		var i = page.indexOf(find);
-		var j = find.length;
-		page = page.substr(0,i) + repl + page.substr(i+j);
-		document.body.innerHTML = page;
-	}
-	
-	var find = "Utilizing";
-	var repl = "Using";
-	var page = document.body.innerHTML;
-	while (page.indexOf(find) >= 0) {
-		var i = page.indexOf(find);
-		var j = find.length;
-		page = page.substr(0,i) + repl + page.substr(i+j);
-		document.body.innerHTML = page;
-	}
-	
-	var find = "utilizable";
-	var repl = "usable";
-	var page = document.body.innerHTML;
-	while (page.indexOf(find) >= 0) {
-		var i = page.indexOf(find);
-		var j = find.length;
-		page = page.substr(0,i) + repl + page.substr(i+j);
-		document.body.innerHTML = page;
-	}
-	
-	var find = "Utilizable";
-	var repl = "Usable";
-	var page = document.body.innerHTML;
-	while (page.indexOf(find) >= 0) {
-		var i = page.indexOf(find);
-		var j = find.length;
-		page = page.substr(0,i) + repl + page.substr(i+j);
-		document.body.innerHTML = page;
-	}
-	
-	var find = "utilization";
-	var repl = "use";
-	var page = document.body.innerHTML;
-	while (page.indexOf(find) >= 0) {
-		var i = page.indexOf(find);
-		var j = find.length;
-		page = page.substr(0,i) + repl + page.substr(i+j);
-		document.body.innerHTML = page;
-	}
-	
-	var find = "Utilization";
-	var repl = "Use";
-	var page = document.body.innerHTML;
-	while (page.indexOf(find) >= 0) {
-		var i = page.indexOf(find);
-		var j = find.length;
-		page = page.substr(0,i) + repl + page.substr(i+j);
-		document.body.innerHTML = page;
-	}
 }
+
 
 deutilize();
